3aced642d8f091d6d493550f8f717df79c5c1b0f
maxoumax <131320427+Lexika-Project@users.noreply.github.com>
2023-05-08 14:35:36 +1100

    Update home.js

--- site/static/js/home.js
+++ site/static/js/home.js
@@ -229,12 +229,30 @@ async function main() {
 }
 
 
+// ...
+
 const regexCommands = [
-	{expression: '.', description: "Correspond à n'importe quel caractère sauf un saut de ligne"},
-	{expression: '*', description: 'Répète le caractère précédent zéro ou plusieurs fois'},
-	{expression: '+', description: 'Répète le caractère précédent une ou plusieurs fois'},
-	// Add more regex expressions and their descriptions here
+	{expression: '.', description: "Correspond à n'importe quel caractère sauf un saut de ligne", example: 'a.o → ao, abo, a2o'},
+	{expression: '*', description: 'Répète le caractère précédent zéro ou plusieurs fois', example: 'ab*c → ac, abc, abbc'},
+	{expression: '+', description: 'Répète le caractère précédent une ou plusieurs fois', example: 'ab+c → abc, abbc'},
+	{expression: '?', description: 'Rend le caractère précédent facultatif (0 ou 1 fois)', example: 'ab?c → ac, abc'},
+	{expression: '^', description: 'Correspond au début de la chaîne', example: '^abc → abc, abcdef'},
+	{expression: '$', description: 'Correspond à la fin de la chaîne', example: 'abc$ → abc, defabc'},
+	{expression: '{n}', description: 'Répète le caractère précédent exactement n fois', example: 'a{3} → aaa'},
+	{expression: '{n,}', description: 'Répète le caractère précédent au moins n fois', example: 'a{2,} → aa, aaa'},
+	{expression: '{n,m}', description: 'Répète le caractère précédent entre n et m fois', example: 'a{2,3} → aa, aaa'},
+	{expression: '[abc]', description: 'Correspond à un des caractères entre les crochets', example: 'a[bc] → ab, ac'},
+	{expression: '[^abc]', description: 'Correspond à tout caractère sauf ceux entre les crochets', example: 'a[^bc] → ad, ae'},
+	{expression: '(a|b)', description: "Correspond à l'un des éléments séparés par le symbole |", example: '(ab|cd) → ab, cd'},
+	{expression: '\d', description: 'Correspond à un chiffre (équivalent à [0-9])', example: 'a\\d → a0, a1, a9'},
+	{expression: '\D', description: "Correspond à un caractère qui n'est pas un chiffre", example: 'a\\D → aa, a%, a-'},
+	{expression: '\w', description: 'Correspond à un caractère alphanumérique ou un tiret bas', example: 'a\\w → aa, a1, a_'},
+	{expression: '\W', description: "Correspond à un caractère qui n'est pas alphanumérique", example: 'a\\W → a!, a%, a@'},
+	{expression: '\s', description: 'Correspond à un espace, un tab ou un saut de ligne', example: 'a\\s → a , a\\t, a\\n'},
+	{expression: '\S', description: "Correspond à un caractère qui n'est pas un espace", example: 'a\\S → aa, a1, a!'},
+	// Add more regex expressions, their descriptions, and examples here
 ];
+
 
 const regexCommandsContainer = document.getElementById('regex-commands');
 
@@ -249,8 +267,11 @@ function displayRegexCommands() {
 	headerExpression.innerText = 'Expression';
 	const headerDescription = document.createElement('th');
 	headerDescription.innerText = 'Description';
+	const headerExample = document.createElement('th');
+	headerExample.innerText = 'Exemple';
 	headerRow.appendChild(headerExpression);
 	headerRow.appendChild(headerDescription);
+	headerRow.appendChild(headerExample);
 	thead.appendChild(headerRow);
 	table.appendChild(thead);
 
@@ -261,15 +282,18 @@ function displayRegexCommands() {
 		// Create a table row for each command
 		const row = document.createElement('tr');
 
-		// Create table cells for expression and description
+		// Create table cells for expression, description, and example
 		const expressionCell = document.createElement('td');
 		expressionCell.innerText = command.expression;
 		const descriptionCell = document.createElement('td');
 		descriptionCell.innerText = command.description;
+		const exampleCell = document.createElement('td');
+		exampleCell.innerText = command.example;
 
 		// Append table cells to the row
 		row.appendChild(expressionCell);
 		row.appendChild(descriptionCell);
+		row.appendChild(exampleCell);
 
 		// Append the row to the table body
 		tbody.appendChild(row);
