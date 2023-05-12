876d0a6b53b5e4b4f55c9c46064c7388dfa73bb3
Florian-Audouard <nairolf.audouard@gmail.com>
2023-05-12 14:20:27 +1100

    petite modif serveur

--- site/server.py
+++ site/server.py
@@ -114,5 +114,4 @@ def receive_audio():  # pylint: disable=missing-function-docstring
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(port=80)
+    app.run(host="0.0.0.0", port=80)
