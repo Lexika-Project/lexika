b02ab1d301e1c7f2da1395038a70e5bc6e3cda20
maxoumax <131320427+Lexika-Project@users.noreply.github.com>
2023-05-25 16:52:16 +1100

    Update function.js

--- site/static/js/function.js
+++ site/static/js/function.js
@@ -16,8 +16,10 @@ export function createTableResult(tab, langueBase, listeLangue, resultTitle, res
 		th = document.createElement("th");
 		thx = document.createElement("th");
 		th.innerHTML = `${langue}`;
+		th.setAttribute("onclick", `sortTable(${index})`); // ajout de l'attribut onclick
 		trTitle.appendChild(th);
 		trfoot.appendChild(thx);
+		index++;
 	}
 
 	resultTitle.appendChild(trTitle);
@@ -66,7 +68,28 @@ export function createTableResult(tab, langueBase, listeLangue, resultTitle, res
 	}
 }
 
-
+function sortTable(n) {
+	var table, rows, switching, i, x, y, shouldSwitch;
+	table = document.getElementById("table");
+	switching = true;
+	while (switching) {
+	  switching = false;
+	  rows = table.rows;
+	  for (i = 1; i < (rows.length - 1); i++) {
+		shouldSwitch = false;
+		x = rows[i].getElementsByTagName("TD")[n];
+		y = rows[i + 1].getElementsByTagName("TD")[n];
+		if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+		  shouldSwitch = true;
+		  break;
+		}
+	  }
+	  if (shouldSwitch) {
+		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		switching = true;
+	  }
+	}
+}
 
 function playSound(event) {
 	let button = event.target;
